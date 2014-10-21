26a64df92c58e4f7594904d4fdc08df5c5e72165
arg0 <zzhhgo@gmail.com>
2014-10-22 00:09:31 +0800

    half

--- codility1.cpp
+++ codility1.cpp
@@ -7,23 +7,36 @@ struct Node{
     int id;
     int dur;
     int weight;
-    Node *parent;
-    vector<int> children=new vector<int>(n);
+    Node *parent=NULL;
+    vector<int> children;
+    
+    public:
+    	Node(int i,int d,int w){
+    		id=i;dur=d;weight=w;parent=nullptr;
+    	}
+    	Node(){
+    		id:0;
+    		dur:0;
+    		weight:0;
+    		parent:nullptr;
+    	}
 };
 int solution(vector<int> &A, vector<int> &B, vector<int> &C) {
     int N=A.size();
     if(N==0) return 0;
-    Node nodes[]=new Node[N];
-    Node root=new Node(-1,0,0);
+    Node nodes[N];
+    Node root={-1,0,0};
     int i=0;
     for(i=0;i<N;i++){
-        Node node=new Node(i,A[i],B[i]);
+        Node node(i,A[i],B[i]);
         if(C[i]==-1){
-            node.parent=root;
+            node.parent=&root;
         }else{
-            node.parent=nodes[C[i]];
+            node.parent=&nodes[C[i]];
         }
-        node.parent.children.push_back(i);
+        node.parent->children.push_back(i);
         nodes[i]=node;        
     }
+    
+    return 0;
 }
